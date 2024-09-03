ca0d291f13423e6093f25dc776bfb190431604d8
anit-agarwal-sccgov <76703323+anit-agarwal-sccgov@users.noreply.github.com>
2024-09-03 16:22:23 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMember() {
+    var member = this;
+    member.skills = [
+        { name: 'JavaScript', level: 'Advanced' },
+        { name: 'HTML', level: 'Advanced' },
+        { name: 'CSS', level: 'Advanced' },
+        { name: 'AngularJS', level: 'Advanced' },
+        { name: 'Node.js', level: 'Intermediate' },
+        { name: 'MongoDB', level: 'Intermediate' },
+        { name: 'Java', level: 'Intermediate' },
+        { name: 'SQL', level: 'Intermediate' }
+    ];
+}
